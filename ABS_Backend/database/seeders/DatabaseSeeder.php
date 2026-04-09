4e6b2c4f3d7419b5f33cee9f9b02b84cd460c58d
Sirius Leaf <hfzamdhai@gmail.com>
2026-04-09 12:49:54 +0700

    Update DatabaseSeeder.php

--- ABS_Backend/database/seeders/DatabaseSeeder.php
+++ ABS_Backend/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ use App\Models\Pengepul;
 use App\Models\Petugas;
 use App\Models\Manager;
 use App\Models\Admin;
-use App\Models\Penjemputan;
+use App\Models\KategoriSampah;
+use App\Models\ItemSampah;
+use App\Models\Sampah;
 
 class DatabaseSeeder extends Seeder
 {
@@ -35,6 +37,234 @@ class DatabaseSeeder extends Seeder
         Gudang::create([
             'alamat' => 'Surakarta',
         ]);
+
+        $dataKategori = [
+            ['nama' => 'Plastik'],
+            ['nama' => 'Kertas'],
+            ['nama' => 'Residu'],
+            ['nama' => 'Beling'],
+            ['nama' => 'Logam'],
+            ['nama' => 'Rosok'],
+            ['nama' => 'Elektronik']
+        ];
+
+        foreach ($dataKategori as $item) {
+            KategoriSampah::create($item);
+        }
+
+        $dataItem = [
+            [
+                'nama' => 'Kresek',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '1',
+            ],
+            [
+                'nama' => 'Putihan',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '1',
+            ],
+            [
+                'nama' => 'Bening',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '1',
+            ],
+            [
+                'nama' => 'Multilayer',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '1',
+            ],
+            [
+                'nama' => 'Duplek',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '2',
+            ],
+            [
+                'nama' => 'Putihan',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '2',
+            ],
+            [
+                'nama' => 'Minyak',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '2',
+            ],
+            [
+                'nama' => 'Sterofom',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '3',
+            ],
+            [
+                'nama' => 'Tisu',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '3',
+            ],
+            [
+                'nama' => 'Sapon',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '3',
+            ],
+            [
+                'nama' => 'Kain',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '3',
+            ],
+            [
+                'nama' => 'Botol Bening',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '4',
+            ],
+            [
+                'nama' => 'Botol Gelap',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '4',
+            ],
+            [
+                'nama' => 'Pecahan Kaca',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '4',
+            ],
+            [
+                'nama' => 'Aluminium',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '5',
+            ],
+            [
+                'nama' => 'Kaleng',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '5',
+            ],
+            [
+                'nama' => 'Besi',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '5',
+            ],
+            [
+                'nama' => 'Tembaga',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '5',
+            ],
+            [
+                'nama' => 'Botol Air',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '6',
+            ],
+            [
+                'nama' => 'Plastik Keras',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '6',
+            ],
+            [
+                'nama' => 'Handphone',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '7',
+            ],
+            [
+                'nama' => 'Kabel',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '7',
+            ],
+            [
+                'nama' => 'Charger',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '7',
+            ],
+            [
+                'nama' => 'Computer',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '7',
+            ],
+            [
+                'nama' => 'TV/Radio',
+                'harga_beli' => '500',
+                'harga_jual' => '1000',
+                'diskon' => '0',
+                'kategori_id' => '7',
+            ],
+        ];
+
+        foreach ($dataItem as $item) {
+            ItemSampah::create($item);
+        }
+
+        $dataSampah = [
+            [
+                'stok' => 100,
+                'item_id' => 1,
+                'gudang_id' => 1,
+            ],
+            [
+                'stok' => 100,
+                'item_id' => 5,
+                'gudang_id' => 1,
+            ],
+            [
+                'stok' => 100,
+                'item_id' => 8,
+                'gudang_id' => 1,
+            ],
+            [
+                'stok' => 100,
+                'item_id' => 12,
+                'gudang_id' => 1,
+            ],
+            [
+                'stok' => 100,
+                'item_id' => 15,
+                'gudang_id' => 1,
+            ],
+        ];
+
+        foreach ($dataSampah as $item) {
+            Sampah::create($item);
+        }
 
         Nasabah::create([
             'nama' => 'Nasabah',
