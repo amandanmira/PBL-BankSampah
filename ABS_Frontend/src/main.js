f8db4f8b617efe19dab23d9e56c61095b231cd3f
raditya-hafid <radityahafid123@gmail.com>
2026-06-24 13:15:14 +0700

    update scaramble

--- ABS_Frontend/src/main.js
+++ ABS_Frontend/src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import router from './router'
 
 // Set alamat dasar ke Laravel-mu
-//axios.defaults.baseURL = 'https://api.tabungansampah.online';
+// axios.defaults.baseURL = 'https://api.tabungansampah.online';
 axios.defaults.baseURL = 'http://localhost:8000';
 // INI WAJIB TRUE! Agar browser mau mengirim/menerima Cookie Sanctum
 axios.defaults.withCredentials = true;
